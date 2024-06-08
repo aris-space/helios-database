e1b1d91a1c0de108e8a13af4fa76199a7bbb2b11
Georg Teufelberger <tgeorg@student.ethz.ch>
2024-06-08 16:55:09 +0200

    feat: Generate secret dynamically We don't need to hardcode it as it's fine to change between server restarts. It's only used generate the cookie so on server restart the user just has to re-enter the password to get new valid cookie which is acceptable and preferred over admin forgetting to set a proper secret.

--- src/rosbag-uploader/rosbag_uploader/main.py
+++ src/rosbag-uploader/rosbag_uploader/main.py
@@ -10,10 +10,11 @@ from functools import wraps
 import os
 import datetime
 import logging
+import secrets
 
 app = Flask(__name__)
 
-app.secret_key = "CHANGE_ME"  # Change this to a secure secret key
+app.secret_key = secrets.token_hex(16)  # Generate a random 32-character hex string
 UPLOAD_FOLDER = "uploads"
 
 # Set the maximum file size to 50 GB
